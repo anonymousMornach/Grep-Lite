6b2ef5a26fa57a37d86eb1fa8cc65535fb6a4bdb
Ifedayo <nou213090168@noun.edu.ng>
2023-12-23 04:59:23 +0100

    fix: added manual searching

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,13 @@ fn main() {
     let quote = "Every face, every shop, bedroom window, public-house, and
 dark square is a picture feverishly turned--in search of what?
 It is the same with books. What do we seek through millions of pages?";
-
-for line in quote.lines()
+    pattern(&search_term, &quote);
+    manual(&search_term, &quote);
+}
+//using pattern
+fn pattern(search_term:&str, quote:&str)
+{
+    for line in quote.lines()
 {
     if line.contains(search_term)
     {
@@ -12,4 +17,20 @@ for line in quote.lines()
     }
 }
 
+}
+
+
+//manual method
+fn manual(search_term:&str, quote:&str)
+{
+    let mut line_num:usize = 1;
+    for line in quote.lines()
+    {
+
+        if line.contains(search_term)
+        {
+            println!("{}: {}", line_num, line);
+        }
+        line_num += 1;
+    }
 }
